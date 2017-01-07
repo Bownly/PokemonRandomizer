ab631bec0a64741ce6ab8f3956a67299549f8376
bownly <kopec@mail.usf.edu>
2017-01-07 15:56:39 -0500

    Initial commit

---
+++ Prism/README.md
@@ -0,0 +1,61 @@
+# Pokemon Prism Randomizer
+
+This script randomizes which pokemon are encountered on which routes. It swaps pokemon by family groupings of the same size. The swaps aren't guaranteed to be reciprocal, but the changes will remain consistent throughout every route. To illustrate: You could end up with a result where Venonat/Venomoth get replaced with Onix/Steelix, and Onix/Steelix gets replaced with Tangela/Tangrowth and Tangela/Tangrowth gets replaced with... well, you get the point. In vanilla Prism, you can catch Venonat in the Acqua Mines and the Health Gym, and Venomoth in Castro Forest and Eagulou Park. Given the same example as before, you would instead encounter Onix in the Acqua mines and the Health Gym, and Steelix in Castro Forest and Eagulou Park. And you'd find Tangela wherever Onix are found in vanilla Prism, etc.
+
+
+# How to use
+
+Note: You have to supply your own copy of the Prism source code for this to work. Put the Prism source folder in the same directory as randomize.py, and then run randomize.py. Afterwards, build the Prism ROM as you normally would.
+
+
+# Notes/exceptions
+
+-Pokemon families that can't be caught are left unchanged (starters, gifts, fossils, etc)
+-Legendaries are left unchanged
+-Fire families only swap with other fire families, and the same for electric families (this is to prevent a scenario in which you can't catch a fire and/or electric pokemon before that one early cave)
+--Except Torkoal who is the only fire or electric with a type 1 stage family, and is therefore shuffled with all other 1 stage families
+-Snorunt/Froslass are treated as a 2 stage family, omitting Glalie because I had to omit one and Froslass is rarer
+-Ralts/Kirlia/Gallade are treated as a 3 stage family, omitting Gardevoir because I had to omit one and Gallade is rarer
+-Eevee/Glaceon/Leafeon are treated as a 3 stage family (since you can catch wild Glaceon and Leafeon in vanilla Prism)
+-Slowpoke/Slowking are treated as a 2 stage family, omitting Slowbro because I had to omit one and Slowking is rarer
+-Baby pokemon are omitted, treating their families as one size shorter than normal (ex: Pikachu/Raichu becomes a 2 stage family)
+--Except the Chingling family is treated as 2 stages since you can catch Chingling in the wild in vanilla Prism
+--Also except Hitmonchan/Hitmonlee/Hitmontop are treated as 3 separate 1 stage familes, and Tyrogue is omitted
+
+
+# Full list of non-shuffled pokemon
+
+Chikorita/Bayleef/Meganium
+Totodile/Croconaw/Feraligatr
+Cyndaquil/Quilava/Typhlosion
+Glalie
+Gardevoir
+Slowbro
+Pichu/Tyrogue/Elekid/Magby
+Togepi/Togetic/Togekiss
+Cranidos/Rampardos
+Shieldon/Bastiodon
+Lileep/Cradily
+Anorith/Armaldo
+Bulbasaur/Ivysaur/Venusaur
+Charmander/Charmeleon/Charizard
+Squirtle/Wartortle/Blastoise
+Phancero
+Phancero/Articuno/Zapdos/Moltres
+Mewtwo/Mew
+Lugia/Ho-oh
+Groudon/Kyogre/Rayquaza
+Varaneous
+Raiwato
+Libabeel
+
+
+# Possible future features
+
+-Include gift/starter/fossil pokemon
+-Adapt the script for RBGY/GSC/Brown/etc
+-Have the code output an ips instead of requiring the users to build their own ROM
+-Add some options
+
+
+
---
+++ Prism/randomize.py
@@ -0,0 +1,277 @@
+import codecs
+import os
+import sys
+from random import randint
+
+famlist1 = [
+    ["VOLBEAT"],
+    ["ILLUMISE"],
+    ["HITMONLEE"],
+    ["HITMONCHAN"],
+    ["HITMONTOP"],
+    ["KANGASKHAN"],
+    ["LUNATONE"],
+    ["SOLROCK"],
+    ["SABLEYE"],
+    ["SPIRITOMB"],
+    ["DITTO"],
+    ["SNORLAX"],
+    ["TORKOAL"],
+    ["MAWILE"],
+    ["RELICANTH"],
+    ["ABSOL"],
+    ["CARNIVINE"]
+]
+famlist2 = [
+    ["SNORUNT", "GLALIE"],
+    ["GLIGAR", "GLISCOR"],
+    ["TAILLOW", "SWELLOW"],
+    ["SENTRET", "FURRET"],
+    ["CACNEA", "CACTURNE"],
+    ["JIGGLYPUFF", "WIGGLYTUFF"],
+    ["PARAS", "PARASECT"],
+    ["NATU", "XATU"],
+    ["BRONZOR", "BRONZONG"],
+    ["YANMA", "YANMEGA"],
+    ["FEEBAS", "MILOTIC"],
+    ["SLOWPOKE", "SLOWKING"],
+    ["TEDDIURSA", "URSARING"],
+    ["ONIX", "STEELIX"],
+    ["VENONAT", "VENOMOTH"],
+    ["SKORUPI", "DRAPION"],
+    ["SPINARAK", "ARIADOS"],
+    ["SCYTHER", "SCIZOR"],
+    ["KOFFING", "WEEZING"],
+    ["MAGIKARP", "GYARADOS"],
+    ["MISDREAVUS", "MISMAGIUS"],
+    ["CHANSEY", "BLISSEY"],
+    ["PHANPY", "DONPHAN"],
+    ["MAKUHITA", "HARIYAMA"],
+    ["PINECO", "FORRETRESS"],
+    ["EXEGGCUTE", "EXEGGUTOR"],
+    ["MARILL", "AZUMARILL"],
+    ["TENTACOOL", "TENTACRUEL"],
+    ["DRIFLOON", "DRIFBLIM"],
+    ["SHUPPET", "BANETTE"],
+    ["DUSKULL", "DUSCLOPS"],
+    ["CHINGLING", "CHIMECHO"],
+    ["SPEAROW", "FEAROW"],
+    ["WAILMER", "WAILORD"],
+    ["SURSKIT", "MASQUERAIN"],
+    ["SHROOMISH", "BRELOOM"],
+    ["SWABLU", "ALTARIA"],
+    ["TANGELA", "TANGROWTH"],
+    ["GOLDEEN", "SEAKING"],
+    ["SNEASEL", "WEAVILE"],
+    ["RIOLU", "LUCARIO"],
+    ["BUNEARY", "LOPUNNY"]
+]
+famlist3 = [
+    ["BAGON", "SHELGON", "SALAMENCE"],
+    ["GEODUDE", "GRAVELER", "GOLEM"],
+    ["ZUBAT", "GOLBAT", "CROBAT"],
+    ["PIDGEY","PIDGEOTTO", "PIDGEOT"],
+    ["LOTAD", "LOMBRE", "LUDICOLO"],
+    ["CATERPIE", "METAPOD", "BUTTERFREE"],
+    ["RALTS", "KIRLIA", "GALLADE"],
+    ["ABRA", "KADABRA", "ALAKAZAM"],
+    ["EEVEE", "LEAFEON", "GLACEON"],
+    ["MACHOP", "MACHOKE", "MACHAMP"],
+    ["BELLSPROUT", "WEEPINBELL", "VICTREEBEL"],
+    ["SWINUB", "PILOSWINE", "MAMOSWINE"],
+    ["ARON", "LAIRON", "AGGRON"],
+    ["WHISMUR", "LOUDRED", "EXPLOUD"],
+    ["RHYHORN", "RHYDON", "RHYPERIOR"],
+    ["GASTLY", "HAUNTER", "GENGAR"],
+    ["TRAPINCH", "VIBRAVA", "FLYGON"],
+    ["PORYGON", "PORYGON2", "PORYGONZ"],
+    ["GIBLE", "GABITE", "GARCHOMP"],
+    ["BELDUM", "METANG", "METAGROSS"]
+]
+fire_famlist2 = [
+    ["VULPIX", "NINETALES"],
+    ["HOUNDOUR", "HOUNDOOM"],
+    ["SLUGMA", "MAGCARGO"],
+    ["NUMEL", "CAMERUPT"],
+    ["PONYTA", "RAPIDASH"],
+    ["GROWLITHE", "ARCANINE"],
+    ["MAGMAR", "MAGMORTAR"]
+]
+elec_famlist2 = [
+    ["PIKACHU", "RAICHU"],
+    ["ELECTRIKE", "MANECTRIC"],
+    ["ELECTABUZZ", "ELECTIVIRE"],
+    ["CHINCHOU", "LANTURN"]
+]
+elec_famlist3 = [
+    ["SHINX", "LUXIO", "LUXRAY"],
+    ["MAREEP", "FLAAFFY", "AMPHAROS"],
+    ["MAGNEMITE", "MAGNETON", "MAGNEZONE"]
+]
+
+base_dex = {
+    "BAGON"       : ["BAGON", "SHELGON", "SALAMENCE"],
+    "SNORUNT"     : ["SNORUNT", "GLALIE"],
+    "GEODUDE"     : ["GEODUDE", "GRAVELER", "GOLEM"],
+    "ZUBAT"       : ["ZUBAT", "GOLBAT", "CROBAT"],
+    "PIDGEY"      : ["PIDGEY","PIDGEOTTO", "PIDGEOT"],
+    "LOTAD"       : ["LOTAD", "LOMBRE", "LUDICOLO"],
+    "GLIGAR"      : ["GLIGAR", "GLISCOR"],
+    "CATERPIE"    : ["CATERPIE", "METAPOD", "BUTTERFREE"],
+    "TAILLOW"     : ["TAILLOW", "SWELLOW"],
+    "RALTS"       : ["RALTS", "KIRLIA", "GALLADE"],
+    "SENTRET"     : ["SENTRET", "FURRET"],
+    "CACNEA"      : ["CACNEA", "CACTURNE"],
+    "ABRA"        : ["ABRA", "KADABRA", "ALAKAZAM"],
+    "JIGGLYPUFF"  : ["JIGGLYPUFF", "WIGGLYTUFF"],
+    "PARAS"       : ["PARAS", "PARASECT"],
+    "NATU"        : ["NATU", "XATU"],
+    "BRONZOR"     : ["BRONZOR", "BRONZONG"],
+    "YANMA"       : ["YANMA", "YANMEGA"],
+    "EEVEE"       : ["EEVEE", "LEAFEON", "GLACEON"],
+    "FEEBAS"      : ["FEEBAS", "MILOTIC"],
+    "MACHOP"      : ["MACHOP", "MACHOKE", "MACHAMP"],
+    "BELLSPROUT"  : ["BELLSPROUT", "WEEPINBELL", "VICTREEBEL"],
+    "SLOWPOKE"    : ["SLOWPOKE", "SLOWKING"],
+    "TEDDIURSA"   : ["TEDDIURSA", "URSARING"],
+    "ONIX"        : ["ONIX", "STEELIX"],
+    "VENONAT"     : ["VENONAT", "VENOMOTH"],
+    "VOLBEAT"     : ["VOLBEAT"],
+    "ILLUMISE"    : ["ILLUMISE"],
+    "SKORUPI"     : ["SKORUPI", "DRAPION"],
+    "SPINARAK"    : ["SPINARAK", "ARIADOS"],
+    "SCYTHER"     : ["SCYTHER", "SCIZOR"],
+    "HITMONLEE"   : ["HITMONLEE"],
+    "HITMONCHAN"  : ["HITMONCHAN"],
+    "HITMONTOP"   : ["HITMONTOP"],
+    "KOFFING"     : ["KOFFING", "WEEZING"],
+    "MAGIKARP"    : ["MAGIKARP", "GYARADOS"],
+    "SWINUB"      : ["SWINUB", "PILOSWINE", "MAMOSWINE"],
+    "MISDREAVUS"  : ["MISDREAVUS", "MISMAGIUS"],
+    "CHANSEY"     : ["CHANSEY", "BLISSEY"],
+    "KANGASKHAN"  : ["KANGASKHAN"],
+    "ARON"        : ["ARON", "LAIRON", "AGGRON"],
+    "WHISMUR"     : ["WHISMUR", "LOUDRED", "EXPLOUD"],
+    "RHYHORN"     : ["RHYHORN", "RHYDON", "RHYPERIOR"],
+    "PHANPY"      : ["PHANPY", "DONPHAN"],
+    "LUNATONE"    : ["LUNATONE"],
+    "SOLROCK"     : ["SOLROCK"],
+    "MAKUHITA"    : ["MAKUHITA", "HARIYAMA"],
+    "PINECO"      : ["PINECO", "FORRETRESS"],
+    "EXEGGCUTE"   : ["EXEGGCUTE", "EXEGGUTOR"],
+    "MARILL"      : ["MARILL", "AZUMARILL"],
+    "TENTACOOL"   : ["TENTACOOL", "TENTACRUEL"],
+    "DRIFLOON"    : ["DRIFLOON", "DRIFBLIM"],
+    "SABLEYE"     : ["SABLEYE"],
+    "SPIRITOMB"   : ["SPIRITOMB"],
+    "SHUPPET"     : ["SHUPPET", "BANETTE"],
+    "DUSKULL"     : ["DUSKULL", "DUSCLOPS"],
+    "GASTLY"      : ["GASTLY", "HAUNTER", "GENGAR"],
+    "CHINGLING"   : ["CHINGLING", "CHIMECHO"],
+    "SPEAROW"     : ["SPEAROW", "FEAROW"],
+    "TRAPINCH"    : ["TRAPINCH", "VIBRAVA", "FLYGON"],
+    "WAILMER"     : ["WAILMER", "WAILORD"],
+    "SURSKIT"     : ["SURSKIT", "MASQUERAIN"],
+    "SHROOMISH"   : ["SHROOMISH", "BRELOOM"],
+    "SWABLU"      : ["SWABLU", "ALTARIA"],
+    "TANGELA"     : ["TANGELA", "TANGROWTH"],
+    "GOLDEEN"     : ["GOLDEEN", "SEAKING"],
+    "PORYGON"     : ["PORYGON", "PORYGON2", "PORYGONZ"],
+    "GIBLE"       : ["GIBLE", "GABITE", "GARCHOMP"],
+    "SNEASEL"     : ["SNEASEL", "WEAVILE"],
+    "BELDUM"      : ["BELDUM", "METANG", "METAGROSS"],
+    "DITTO"       : ["DITTO"],
+    "SNORLAX"     : ["SNORLAX"],
+    "TORKOAL"     : ["TORKOAL"],
+    "MAWILE"      : ["MAWILE"],
+    "RELICANTH"   : ["RELICANTH"],
+    "ABSOL"       : ["ABSOL"],
+    "CARNIVINE"   : ["CARNIVINE"],
+    "RIOLU"       : ["RIOLU", "LUCARIO"],
+    "BUNEARY"     : ["BUNEARY", "LOPUNNY"]
+}
+fire_base_dex = {
+    "VULPIX"      : ["VULPIX", "NINETALES"],
+    "HOUNDOUR"    : ["HOUNDOUR", "HOUNDOOM"],
+    "SLUGMA"      : ["SLUGMA", "MAGCARGO"],
+    "NUMEL"       : ["NUMEL", "CAMERUPT"],
+    "PONYTA"      : ["PONYTA", "RAPIDASH"],
+    "GROWLITHE"   : ["GROWLITHE", "ARCANINE"],
+    "MAGMAR"      : ["MAGMAR", "MAGMORTAR"],
+}
+elec_base_dex = {
+    "SHINX"       : ["SHINX", "LUXIO", "LUXRAY"],
+    "MAREEP"      : ["MAREEP", "FLAAFFY", "AMPHAROS"],
+    "PIKACHU"     : ["PIKACHU", "RAICHU"],
+    "MAGNEMITE"   : ["MAGNEMITE", "MAGNETON", "MAGNEZONE"],
+    "ELECTRIKE"   : ["ELECTRIKE", "MANECTRIC"],
+    "ELECTABUZZ"  : ["ELECTABUZZ", "ELECTIVIRE"],
+    "CHINCHOU"    : ["CHINCHOU", "LANTURN"],
+}
+end_dex = {}
+
+
+# these blocks are in charge of determining how the pokemon get switched around
+# majority of the pokemon
+for pokemon in base_dex:
+    ogfam = base_dex[pokemon]
+    famsize = len(ogfam)
+    
+    if (famsize == 1):
+        famlist = famlist1
+    elif (famsize == 2):
+        famlist = famlist2
+    elif (famsize == 3):
+        famlist = famlist3
+        
+    newfam = famlist[randint(0, len(famlist)-1)]
+    for i in range(famsize):
+        end_dex[ogfam[i]] = (newfam[i])
+    famlist.remove(newfam)
+
+# fire pokemon
+for pokemon in fire_base_dex:
+    ogfam = fire_base_dex[pokemon]
+    famsize = 2
+
+    famlist = fire_famlist2
+        
+    newfam = famlist[randint(0, len(famlist)-1)]
+    for i in range(famsize):
+        end_dex[ogfam[i]] = (newfam[i])
+    famlist.remove(newfam)
+    
+# elec pokemon
+for pokemon in elec_base_dex:
+    ogfam = elec_base_dex[pokemon]
+    famsize = len(ogfam)
+
+    if (famsize == 2):
+        famlist = elec_famlist2
+    elif (famsize == 3):
+        famlist = elec_famlist3
+        
+    newfam = famlist[randint(0, len(famlist)-1)]
+    for i in range(famsize):
+        end_dex[ogfam[i]] = (newfam[i])
+    famlist.remove(newfam)
+
+# this block does the actual data replacement in the rom files
+dir = os.path.join( os.getcwd(), 'source','data','wild')
+for f in os.listdir(dir):
+    if f.endswith(".asm"):
+
+        fileObj = codecs.open(dir + "/" + f, "r" )
+        iny = fileObj.read()
+        out = open(dir + "/" + f, 'w')
+
+        for i in end_dex.keys():
+            iny = iny.replace(" "+i+"\n", " "+end_dex[i]+"\n")  # adding the space to prevent false positives like the Marill in Azumarill, and the "\n" is for stuff like Paras and Parasect
+        out.write(iny)
+        fileObj.close()
+        out.close()
+
+
+
+
+
