9d72ecef4376c97953cc95faebe44f990061bd6a
bownly <kopec@mail.usf.edu>
2017-01-08 21:18:39 -0500

    Added gym leader shuffling to the RB script
    See the updated readme for details.

--- RedBlue/README.md
+++ RedBlue/README.md
@@ -1,6 +1,6 @@
 # Pokemon Red/Blue Randomizer
 
-Consult this repository's main README for general details on this script.
+This script randomizes pokemon locations and gym leader order. Consult this repository's main README for general details on this script.
 
 
 # Version compatibility
@@ -15,13 +15,16 @@ Disassembly of [**Pokémon Red/Blue**][pokered]
 Ensure that the folder containing the Red disassembly is named "pokered". Then put the pokered folder in the same directory as randomize.py, and run randomize.py. Afterwards, build the Red and/or Blue ROMs as you normally would. Badabing, badaboom.
 
 
-# Notes/exceptions
+# Pokemon shuffle
+
+
+## Notes/exceptions
 
 - Starter and fossil pokemon families are shuffled, however, the overworld texts etc are not changed to reflect this. Also, your rival's pokemon does not get altered.
 - Trade-exclusive families are not randomized, nor are gift-exclusive families. Same for overworld-exlusive pokemon and Game Corner-exclusive pokemon.
 
 
-# Full list of non-shuffled pokemon
+## Full list of non-shuffled pokemon
 
 - Eevee/Vaporeon/Jolteon/Flareon
 - Jynx
@@ -31,6 +34,91 @@ Ensure that the folder containing the Red disassembly is named "pokered". Then p
 - Snorlax
 - Articuno/Zapdos/Moltres
 - Mewtwo/Mew
+
+
+# Gym leader shuffle
+
+
+## Explanation
+
+This script shuffles around the order of the gym leaders (excluding Giovanni). Their teams are scaled/altered to maintain the same relative balance as in the vanilla experience. 
+
+Things that are shuffled by the script:
+- Gym leader locations/order
+- Gym leader sprites
+
+Things that are not shuffled by the script:
+- Badge order/distribution
+- The order/distribution of the gym TM prizes
+- Any corresponding text/dialog
+
+
+## Full teams listing
+
+I had to make up pretty much all of these teams. They should all be roughly equal in terms of balance. Format is {level} {pokemon}. 
+
+Brock:
+- 12 GEODUDE, 14 ONIX
+- 18 GRAVELER, 21 ONIX
+- 21 GRAVELER, 18 AERODACTYL, 24 ONIX
+- 29 GOLEM, 24 AERODACTYL, 29 ONIX
+- 37 OMANYTE, 39 GOLEM, 37 AERODACTYL, 43 ONIX
+- 38 OMASTAR, 37 GOLEM, 38 AERODACTYL, 43 ONIX
+- 42 OMASTAR, 40 GOLEM, 42 AERODACTYL, 47 ONIX
+
+Misty:
+- 12 PSYDUCK, 14 STARYU
+- 18 PSYDUCK, 21 STARMIE
+- 21 PSYDUCK, 18 LAPRAS, 24 STARMIE
+- 29 GOLDUCK, 24 LAPRAS, 29 STARMIE
+- 37 TENTACOOL, 39 GOLDUCK, 37 LAPRAS, 43 STARMIE
+- 38 TENTACRUEL, 37 GOLDUCK, 38 LAPRAS, 43 STARMIE
+- 42 TENTACRUEL, 40 GOLDUCK, 42 LAPRAS, 47 STARMIE
+
+Lt. Surge:
+- 12 VOLTORB, 14 PIKACHU
+- 18 VOLTORB, 21 RAICHU
+- 21 VOLTORB, 18 ELECTABUZZ, 24 RAICHU
+- 29 ELECTRODE, 24 ELECTABUZZ, 29 RAICHU
+- 37 MAGNEMITE, 39 ELECTRODE, 37 ELECTABUZZ, 43 RAICHU
+- 38 MAGNETON, 37 ELECTRODE, 38 ELECTABUZZ, 43 RAICHU
+- 42 MAGNETON, 40 ELECTRODE, 42 ELECTABUZZ, 47 RAICHU
+
+Erika:
+- 12 BELLSPROUT, 14 ODDISH
+- 18 WEEPINBELL, 21 GLOOM
+- 21 WEEPINBELL, 18 TANGELA, 24 VILEPLUME
+- 29 VICTREEBEL, 24 TANGELA, 29 VILEPLUME
+- 37 EXEGGCUTE, 39 VICTREEBEL, 37 TANGELA, 43 VILEPLUME
+- 38 EXEGGUTOR, 37 VICTREEBEL, 38 TANGELA, 43 VILEPLUME
+- 42 EXEGGUTOR, 40 VICTREEBEL, 42 TANGELA, 47 VILEPLUME
+
+Koga:
+- 12 GRIMER, 14 KOFFING
+- 18 GRIMER, 21 WEEZING
+- 21 GRIMER, 18 GOLBAT, 24 WEEZING
+- 29 MUK, 24 GOLBAT, 29 WEEZING
+- 37 NIDORINO, 39 MUK, 37 GOLBAT, 43 WEEZING
+- 38 NIDOKING, 37 MUK, 38 GOLBAT, 43 WEEZING
+- 42 NIDOKING, 40 MUK, 42 GOLBAT, 47 WEEZING
+
+Sabrina:
+- 12 VENONAT, 14 KADABRA
+- 18 VENONAT, 21 KADABRA
+- 21 VENONAT, 18 MR_MIME, 24 KADABRA
+- 29 VENOMOTH, 24 MR_MIME, 29 ALAKAZAM
+- 37 SLOWPOKE, 39 MR_MIME, 37 VENOMOTH, 43 ALAKAZAM
+- 38 SLOWBRO, 37 MR_MIME, 38 VENOMOTH, 43 ALAKAZAM
+- 42 SLOWBRO, 40 MR_MIME, 42 VENOMOTH, 47 ALAKAZAM
+
+Blaine:
+- 12 PONYTA, 14 GROWLITHE
+- 18 PONYTA, 21 ARCANINE
+- 21 PONYTA, 18 MAGMAR, 24 ARCANINE
+- 29 RAPIDASH, 24 MAGMAR, 29 ARCANINE
+- 37 VULPIX, 39 RAPIDASH, 37 MAGMAR, 43 ARCANINE
+- 38 NINETALES, 37 RAPIDASH, 38 MAGMAR, 43 ARCANINE
+- 42 NINETALES, 40 MAGMAR, 42 RAPIDASH, 47 ARCANINE
 
 
 # Possible future features
--- RedBlue/randomize.py
+++ RedBlue/randomize.py
@@ -1,7 +1,8 @@
 import codecs
 import os
+import re
 import sys
-from random import randint
+from random import randint, shuffle
 
 famlist1 = [
     ["FARFETCHD"],
@@ -186,7 +187,6 @@ files_list.append(dir)
 
 # this block does the actual data replacement in the rom files
 for fl in files_list:
-    print fl
     with open(fl) as f:
         content = [x.strip('\n') for x in f.readlines()]
         out_lines = []
@@ -208,14 +208,259 @@ for fl in files_list:
 
 
 
+# gym leader shuffle starts here
 
-# gym leader stuff to consider:
-    # whatever this stuff is:
-        # pokered/blob/master/data/trainer_moves.asm
-    # TM stuff (for all gyms, not just pewter):
-        # pokered/blob/master/scripts/pewtergym
-    # parties list:
-        # master/data/trainer_parties.asm
+class Brock(object):
+    def __init__ (self, ordinal):
+        self.name = "Brock"
+        self.sprite = 'SPRITE_BLACK_HAIR_BOY_2'
+        self.opp = 'BROCK'
+        self.ordinal = ordinal
+        self.ai_line = '\tdb 1,0    ; BROCK'
+        self.move_list = [  "db 1,BIDE",
+                            "db 1,ROCK_THROW",
+                            "db 2,ROCK_SLIDE",
+                            "db 2,ROCK_SLIDE",
+                            "db 3,ROCK_SLIDE",
+                            "db 3,ROCK_SLIDE",
+                            "db 3,ROCK_SLIDE"]
+        self.team = "BrockData:\n\tdb $FF,"        
+        self.team_list = ["12,GEODUDE,14,ONIX,0",
+                          "18,GRAVELER,21,ONIX,0",
+                          "21,GRAVELER,18,AERODACTYL,24,ONIX,0",
+                          "29,GOLEM,24,AERODACTYL,29,ONIX,0",
+                          "37,OMANYTE,39,GOLEM,37,AERODACTYL,43,ONIX,0",
+                          "38,OMASTAR,37,GOLEM,38,AERODACTYL,43,ONIX,0",
+                          "42,OMASTAR,40,GOLEM,42,AERODACTYL,47,ONIX,0"]
+        self.team += self.team_list[self.ordinal]
+
+class Misty(object):
+    def __init__ (self, ordinal):
+        self.name = "Misty"
+        self.sprite = 'SPRITE_BRUNETTE_GIRL'
+        self.opp = 'MISTY'
+        self.ordinal = ordinal
+        self.ai_line = '\tdb 1,3,0  ; MISTY'
+        self.move_list = [  "db 1,BUBBLE",
+                            "db 1,BUBBLEBEAM",
+                            "db 2,BUBBLEBEAM",
+                            "db 2,BUBBLEBEAM",
+                            "db 3,BUBBLEBEAM",
+                            "db 3,BUBBLEBEAM",
+                            "db 3,BUBBLEBEAM"]
+        self.team = "MistyData:\n    db $FF,"
+        self.team_list = ["12,PSYDUCK,14,STARYU,0",
+                          "18,PSYDUCK,21,STARMIE,0",
+                          "21,PSYDUCK,18,LAPRAS,24,STARMIE,0",
+                          "29,GOLDUCK,24,LAPRAS,29,STARMIE,0",
+                          "37,TENTACOOL,39,GOLDUCK,37,LAPRAS,43,STARMIE,0",
+                          "38,TENTACRUEL,37,GOLDUCK,38,LAPRAS,43,STARMIE,0",
+                          "42,TENTACRUEL,40,GOLDUCK,42,LAPRAS,47,STARMIE,0"]
+        self.team += self.team_list[self.ordinal]
+
+class Surge(object):
+    def __init__ (self, ordinal):
+        self.name = "LtSurge"
+        self.sprite = 'SPRITE_ROCKER'
+        self.opp = 'LT_SURGE'
+        self.ordinal = ordinal
+        self.ai_line = '\tdb 1,3,0  ; LT_SURGE'
+        self.move_list = [  "db 1,THUNDERSHOCK",
+                            "db 1,THUNDERBOLT",
+                            "db 2,THUNDERBOLT",
+                            "db 2,THUNDERBOLT",
+                            "db 3,THUNDERBOLT",
+                            "db 3,THUNDERBOLT",
+                            "db 3,THUNDERBOLT"]
+        self.team = "LtSurgeData:\n\tdb $FF,"
+        self.team_list = ["12,VOLTORB,14,PIKACHU,0",
+                          "18,VOLTORB,21,RAICHU,0",
+                          "21,VOLTORB,18,ELECTABUZZ,24,RAICHU,0",
+                          "29,ELECTRODE,24,ELECTABUZZ,29,RAICHU,0",
+                          "37,MAGNEMITE,39,ELECTRODE,37,ELECTABUZZ,43,RAICHU,0",
+                          "38,MAGNETON,37,ELECTRODE,38,ELECTABUZZ,43,RAICHU,0",
+                          "42,MAGNETON,40,ELECTRODE,42,ELECTABUZZ,47,RAICHU,0"]
+        self.team += self.team_list[self.ordinal]
+
+class Erika(object):
+    def __init__ (self, ordinal):
+        self.name = "Erika"
+        self.sprite = 'SPRITE_ERIKA'
+        self.opp = 'ERIKA'
+        self.ordinal = ordinal
+        self.ai_line = '\tdb 1,3,0  ; ERIKA'
+        self.move_list = [  "db 1,MEGA_DRAIN",
+                            "db 1,MEGA_DRAIN",
+                            "db 2,MEGA_DRAIN",
+                            "db 2,MEGA_DRAIN",
+                            "db 3,MEGA_DRAIN",
+                            "db 3,MEGA_DRAIN",
+                            "db 3,MEGA_DRAIN"]
+        self.team = "ErikaData:\n    db $FF,"
+        self.team_list = ["12,BELLSPROUT,14,ODDISH,0",
+                          "18,WEEPINBELL,21,GLOOM,0",
+                          "21,WEEPINBELL,18,TANGELA,24,VILEPLUME,0",
+                          "29,VICTREEBEL,24,TANGELA,29,VILEPLUME,0",
+                          "37,EXEGGCUTE,39,VICTREEBEL,37,TANGELA,43,VILEPLUME,0",
+                          "38,EXEGGUTOR,37,VICTREEBEL,38,TANGELA,43,VILEPLUME,0",
+                          "42,EXEGGUTOR,40,VICTREEBEL,42,TANGELA,47,VILEPLUME,0"]
+        self.team += self.team_list[self.ordinal]
+
+class Koga(object):
+    def __init__ (self, ordinal):
+        self.name = "Koga"
+        self.sprite = 'SPRITE_BLACKBELT'
+        self.opp = 'KOGA'
+        self.ordinal = ordinal
+        self.ai_line = '\tdb 1,3,0  ; KOGA'
+        self.move_list = [  "db 1,TOXIC",
+                            "db 1,TOXIC",
+                            "db 2,TOXIC",
+                            "db 2,TOXIC",
+                            "db 3,TOXIC",
+                            "db 3,TOXIC",
+                            "db 3,TOXIC"]
+        self.team = "KogaData:\n    db $FF,"
+        self.team_list = ["12,GRIMER,14,KOFFING,0",
+                          "18,GRIMER,21,WEEZING,0",
+                          "21,GRIMER,18,GOLBAT,24,WEEZING,0",
+                          "29,MUK,24,GOLBAT,29,WEEZING,0",
+                          "37,NIDORINO,39,MUK,37,GOLBAT,43,WEEZING,0",
+                          "38,NIDOKING,37,MUK,38,GOLBAT,43,WEEZING,0",
+                          "42,NIDOKING,40,MUK,42,GOLBAT,47,WEEZING,0"]
+        self.team += self.team_list[self.ordinal]
+
+class Sabrina(object):
+    def __init__ (self, ordinal):
+        self.name = "Sabrina"
+        self.sprite = 'SPRITE_GIRL'
+        self.opp = 'SABRINA'
+        self.ordinal = ordinal
+        self.ai_line = '\tdb 1,3,0  ; BLAINE'
+        self.move_list = [  "db 1,PSYWAVE",
+                            "db 1,PSYWAVE",
+                            "db 2,PSYWAVE",
+                            "db 2,PSYWAVE",
+                            "db 3,PSYWAVE",
+                            "db 3,PSYWAVE",
+                            "db 3,PSYWAVE"]
+        self.team = "SabrinaData:\n    db $FF,"
+        self.team_list = ["12,VENONAT,14,KADABRA,0",
+                          "18,VENONAT,21,KADABRA,0",
+                          "21,VENONAT,18,MR_MIME,24,KADABRA,0",
+                          "29,VENOMOTH,24,MR_MIME,29,ALAKAZAM,0",
+                          "37,SLOWPOKE,39,MR_MIME,37,VENOMOTH,43,ALAKAZAM,0",
+                          "38,SLOWBRO,37,MR_MIME,38,VENOMOTH,43,ALAKAZAM,0",
+                          "42,SLOWBRO,40,MR_MIME,42,VENOMOTH,47,ALAKAZAM,0"]
+        self.team += self.team_list[self.ordinal]
+
+class Blaine(object):
+    def __init__ (self, ordinal):
+        self.name = "Blaine"
+        self.sprite = 'SPRITE_FAT_BALD_GUY'
+        self.opp = 'BLAINE'
+        self.ordinal = ordinal
+        self.ai_line = '\tdb 1,3,0  ; SABRINA'
+        self.move_list = [  "db 1,EMBER",
+                            "db 1,FLAMETHROWER",
+                            "db 2,FLAMETHROWER",
+                            "db 2,FIRE_BLAST",
+                            "db 3,FIRE_BLAST",
+                            "db 3,FIRE_BLAST",
+                            "db 3,FIRE_BLAST"]
+        self.team = "BlaineData:\n    db $FF,"
+        self.team_list = ["12,PONYTA,14,GROWLITHE,0",
+                          "18,PONYTA,21,ARCANINE,0",
+                          "21,PONYTA,18,MAGMAR,24,ARCANINE,0",
+                          "29,RAPIDASH,24,MAGMAR,29,ARCANINE,0",
+                          "37,VULPIX,39,RAPIDASH,37,MAGMAR,43,ARCANINE,0",
+                          "38,NINETALES,37,RAPIDASH,38,MAGMAR,43,ARCANINE,0",
+                          "42,NINETALES,40,MAGMAR,42,RAPIDASH,47,ARCANINE,0"]
+        self.team += self.team_list[self.ordinal]
+
+# shuffles the gym leader order
+gyms = [0, 1, 2, 3, 4, 5, 6]
+shuffle(gyms)
+leaders = [Brock(gyms[0]), Misty(gyms[1]), Surge(gyms[2]), Erika(gyms[3]), Koga(gyms[4]), Sabrina(gyms[5]), Blaine(gyms[6])]
+leaders.sort(key=lambda x: x.ordinal)
+for l in leaders:
+    print l.name, l.ordinal
+
+# rewrites the party data for each leader
+dir = os.path.join(os.getcwd(), 'pokered', 'data')
+dir += '/trainer_parties.asm'
+for leader in leaders:
+    with open(dir) as f:
+        content = f.read()
+        out = open(dir, 'w')
+        out_content = re.sub( leader.name+'Data.*\n.*0', leader.team, content)
+        out.write(out_content)
+        out.close()
+
+# swaps out the map sprites and encounter data for each gym
+city_list = ['/pewtergym.asm', '/ceruleangym.asm', '/vermiliongym.asm', 
+             '/celadongym.asm', '/fuchsiagym.asm', '/saffrongym.asm', '/cinnibargym.asm']
+dir = os.path.join(os.getcwd(), 'pokered', 'data', 'mapObjects')
+for city in range(len(city_list)-1):
+    temp_dir = dir + city_list[city]
+    print 'dir: ', temp_dir
+    leader = leaders[city]
+    with open(temp_dir) as f:
+        content = f.read()
+        out = open(temp_dir, 'w')
+        out_content = re.sub( 'object .*?\,', 'object ' + leader.sprite + ',', content, 1)
+        out_content = re.sub( 'OPP_.*,', 'OPP_' + leader.opp + ',', out_content, 1)
+        out.write(out_content)
+        out.close()
+
+# swaps around the special trainer moves
+new_moves = ''
+for leader in leaders:
+    new_moves += ('\t' + leader.move_list[leader.ordinal] + '\n')
+dir = os.path.join(os.getcwd(), 'pokered', 'data')
+dir += '/trainer_moves.asm'
+with open(dir) as f:
+    print 'hi'
+    content = f.read()
+    out = open(dir, 'w')
+    out_content = re.sub('(\tdb \d\,.*\n){7}', new_moves, content)
+    out.write(out_content)
+    out.close()
+
+
+# swaps around the ai
+dir = os.path.join(os.getcwd(), 'pokered', 'engine', 'battle')
+dir += '/trainer_ai.asm'
+
+new_ais = ''  # gotta build out the new_ais string
+for leader in leaders:
+    new_ais += (leader.ai_line + '\n')
+
+og_ais = ''
+with open(dir) as f:  # gotta build out the og_ais string
+    content = f.read()
+    srx = re.search('db.*BRUNO.*\n(.*\n)(.*\n)(.*\n)(.*\n)(.*\n)(.*\n)(.*\n)', content)
+    for g in srx.groups():
+        og_ais += g
+
+with open(dir) as f:
+    content = f.read()
+    out = open(dir, 'w')
+    out_content = re.sub( og_ais, new_ais, content)
+    out.write(out_content)
+    out.close()
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
